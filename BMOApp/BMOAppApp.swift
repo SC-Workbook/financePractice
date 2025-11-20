72feac7849ea6b0a06cb7cba4a138efc2388f777
SahilChowkekar22 <Sahil.Chowkekar@techconsulting.tech>
2025-11-20 13:57:53 -0500

    Added initial modular structure: CoreKit, NetworkKit, DesignSystem, and Feature Modules

---
+++ BMOApp/BMOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMOAppApp.swift
+//  BMOApp
+//
+//  Created by Sahil ChowKekar on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BMOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
